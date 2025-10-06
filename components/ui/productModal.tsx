638ebf19c53b95e13ea816f28c248fb8709b8fed
Elias palacio <gamerpg08@gmail.com>
2025-10-05 20:32:55 -0500

    details

--- components/ui/productModal.tsx
+++ components/ui/productModal.tsx
@@ -163,7 +163,7 @@ export default function ProductModal() {
               </svg>
             </button>
 
-            {showAbout && (
+            {!showAbout && (
               <div className="pt-4 space-y-4 text-sm text-gray-700">
                 <div>
                   <ul className="space-y-1">
